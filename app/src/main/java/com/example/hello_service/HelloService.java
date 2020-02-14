956abb0e3187ac03bf9e77c36088fa97ec30c452
essarraj.fouad <essarraj.fouad@gmail.com>
2020-02-14 11:19:57 +0100

    add service

--- app/src/main/java/com/example/hello_service/HelloService.java
+++ app/src/main/java/com/example/hello_service/HelloService.java
@@ -3,14 +3,31 @@ package com.example.hello_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class HelloService extends Service {
     public HelloService() {
+
+    }
+
+   // @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/hello_service/MainActivity.java
+++ app/src/main/java/com/example/hello_service/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hello_service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), HelloService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), HelloService.class));
     }
 }
